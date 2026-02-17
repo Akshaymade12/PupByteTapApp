4eafca971c4eb289479c6b67502ef8b65879b8dc
Akshaymade12 <adultgang5@gmail.com>
2026-02-18 04:15:43 +0530

    Fix save API

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ function tap(){
   coins += 1;
   document.getElementById("coins").innerText = coins;
 }
+async function saveCoins(telegramId, coins) {
+  try {
+    await fetch("https://pupbytetapapp.onrender.com", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        telegramId: telegramId,
+        coins: coins
+      })
+    });
+  } catch (err) {
+    console.log("Save error:", err);
+  }
+}
