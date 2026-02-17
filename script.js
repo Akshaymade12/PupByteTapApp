4bf2a7a5a1d44aff13db4d25df0cda2650963617
Akshaymade12 <adultgang5@gmail.com>
2026-02-18 04:30:41 +0530

    Fixed coin save system

--- script.js
+++ script.js
@@ -1,22 +1,38 @@
 let coins = 0;
 
-function tap(){
+function tap() {
   coins += 1;
   document.getElementById("coins").innerText = coins;
+
+  saveCoins();
 }
-async function saveCoins(telegramId, coins) {
-  try {
-    await fetch("https://pupbytetapapp.onrender.com", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        telegramId: telegramId,
-        coins: coins
-      })
-    });
-  } catch (err) {
-    console.log("Save error:", err);
-  }
+
+async function saveCoins() {
+  let telegramId = window.Telegram.WebApp.initDataUnsafe.user.id;
+
+  await fetch("https://pupbytebot.onrender.com", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      telegramId: telegramId,
+      coins: coins,
+    }),
+  });
 }
+
+async function loadCoins() {
+  let telegramId = window.Telegram.WebApp.initDataUnsafe.user.id;
+
+  let res = await fetch(
+    "https://pupbytebot.onrender.com" + telegramId
+  );
+
+  let data = await res.json();
+
+  coins = data.coins;
+  document.getElementById("coins").innerText = coins;
+}
+
+window.onload = loadCoins;
