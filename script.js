c9459b8715c0f0ccca212cdda41d68cb52c98dbf
Akshaymade12 <adultgang5@gmail.com>
2026-02-18 05:50:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 let coins = 0;
 
+const API = "https://pupbytetapapp.onrender.com";
+
+function getTelegramId() {
+  if (window.Telegram && window.Telegram.WebApp) {
+    return window.Telegram.WebApp.initDataUnsafe.user.id;
+  }
+
+  // Backup ID (testing ke liye)
+  return "test123";
+}
+
+// Tap
 function tap() {
   coins += 1;
   document.getElementById("coins").innerText = coins;
@@ -7,31 +19,35 @@ function tap() {
   saveCoins();
 }
 
+// Save
 async function saveCoins() {
-  let telegramId = window.Telegram.WebApp.initDataUnsafe.user.id;
 
-  await fetch("https://pupbytebot.onrender.com/save", {
+  let telegramId = getTelegramId();
+
+  console.log("Saving:", telegramId, coins);
+
+  await fetch(API + "/save", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      telegramId: telegramId,
-      coins: coins,
+      telegramId,
+      coins
     }),
   });
 }
 
+// Load
 async function loadCoins() {
-  let telegramId = window.Telegram.WebApp.initDataUnsafe.user.id;
 
-  let res = await fetch(
-    "https://pupbytebot.onrender.com/load/" + telegramId
-  );
+  let telegramId = getTelegramId();
+
+  let res = await fetch(API + "/load/" + telegramId);
 
   let data = await res.json();
 
-  coins = data.coins;
+  coins = data.coins || 0;
   document.getElementById("coins").innerText = coins;
 }
 
