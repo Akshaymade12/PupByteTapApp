f3fdc3e3bf927b2ec8b435aa7f3358e59f2b467e
Akshaymade12 <adultgang5@gmail.com>
2026-03-03 23:53:28 +0530

    Loading page script

--- script.js
+++ script.js
@@ -574,5 +574,15 @@ if(closeLeagueBtn){
     earnSection.style.display = "block";
   }
 }
+  /* ===== LOADING SCREEN HIDE ===== */
+  
+  window.addEventListener("load", () => {
+  setTimeout(() => {
+    const loader = document.getElementById("loadingScreen");
+    loader.style.opacity = "0";
+    loader.style.visibility = "hidden";
+  }, 1800); // 1.8 sec premium delay
   
 });
+
+  });
