f1efbca9e1cf9747950794789cdc6a5ea9430764
Akshaymade12 <adultgang5@gmail.com>
2026-04-07 18:59:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -644,6 +644,6 @@ cron.schedule("0 0 * * 0", async () => {
 /* ================= START ================= */
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log("Server running 🚀");
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT} 🚀`);
 });
